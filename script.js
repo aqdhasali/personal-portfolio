f5cba858d23b94384c4f5be2fb8fc315f1eb160b
Aqdhas Ali <aqdhasali468@gmail.com>
2024-03-25 19:04:11 +0530

    added responsiveness

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function downloadCV(){
      // Clean up
      document.body.removeChild(link);
 }
+
+// Hamburger Menu
+const nav_btn = document.querySelector('.hamburger');
+const mobile_nav = document.querySelector('.mobile-nav');
+
+nav_btn.addEventListener('click',function(){
+    nav_btn.classList.toggle('is-active'); // To open the hamburger menu when the button is toggled.
+    mobile_nav.classList.toggle('is-active') //To display the content iniside the mobile nav
+
+})
